90813124f41af23c70baf4ec8c096ee97b5ba5f3
kamalpraemkumar <kamal.praem_kumar@hotmail.com>
2016-11-24 17:28:49 +0800

    Final

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mongo = require('mongodb').MongoClient;
 const assert = require('assert');
-const mongourl = 'mongodb://localhost:27017/test';
+const mongourl = 'mongodb://localhost:27017/wbccms';
 
 router.get('/', function(req, res, next) {
     "use strict";
@@ -37,6 +37,7 @@ router.get('/', function(req, res, next) {
 
                         res.render('admin.ejs', {
                             title: req.app.locals.site.title,
+                            rate: req.app.locals.site.rate,
                             dataMachine: dataMachine,
                             dataUser: dataUser,
                             dataTickets: dataTickets,
@@ -49,13 +50,31 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/rate', function(req, res, next) {
+    "use strict";
+    let rate = req.app.locals.site.rate;
+    res.json(rate);
+});
+
+router.post('/rateChange', function(req, res, next) {
+    "use strict";
+    let newrate = req.body.newrate;
+    newrate = newrate.split('&');
+    let rate = newrate[0];
+    rate = rate.substr(rate.indexOf("=") + 1);
+    rate = parseFloat(Math.round(rate * 100) / 100).toFixed(2);
+    console.log('Server Rate', rate);
+    res.app.locals.site.rate = parseFloat(rate);
+    res.send('Success');
+});
+
 router.get('/task', function(req, res, next) {
     "use strict";
     let dataTask = [];
     mongo.connect(mongourl, (err, db) => {
         assert.equal(null, err);
-        let cursorMachine = db.collection('task').find({});
-        cursorMachine.forEach((dbitem, index, arr) => {
+        let cursorTask = db.collection('task').find({});
+        cursorTask.forEach((dbitem, index, arr) => {
             assert.equal(null, index);
             dataTask.push(dbitem);
     }, () => {
@@ -98,9 +117,13 @@ router.post('/taskAdd', function(req, res, next) {
     let prog = formData[1];
     prog = prog.substr(prog.indexOf("=") + 1);
 
+    let dt = new Date();
+    let d = dt.getFullYear() +'-'+ (dt.getMonth()+1) +'-'+ dt.getDate();
+
     mongo.connect(mongourl, (err, db) => {
         assert.equal(null, err);
         db.collection('task').insertOne({
+            date: d,
             taskName: decodeURIComponent(name),
             status: 'Pending',
             taskProgress: prog
@@ -108,8 +131,82 @@ router.post('/taskAdd', function(req, res, next) {
         db.close();
         res.send('Success');
     });
+});
 
+router.get('/tickets', function(req, res, next) {
+    "use strict";
+    let dataTicket = [];
+    mongo.connect(mongourl, (err, db) => {
+        assert.equal(null, err);
+        let cursorTicket = db.collection('tickets').find({});
+        cursorTicket.forEach((dbitem, index, arr) => {
+            assert.equal(null, index);
+            dataTicket.push(dbitem);
+        }, () => {
+            db.close();
+            res.json(dataTicket);
+        });
+    });
+});
 
+router.post('/ticketUpdate', function(req, res, next) {
+    "use strict";
+    let ticketId = req.body.id;
+    ticketId = require('mongodb').ObjectID(ticketId);
+    mongo.connect(mongourl, (err, db) => {
+        assert.equal(null, err);
+        db.collection('tickets').update({_id: ticketId}, {
+            $set: {
+                priority: 'Completed',
+            }
+        }, () => {
+            db.close();
+            res.send('Success');
+        });
+    });
+});
+
+router.post('/ticketAdd', function(req, res, next) {
+    "use strict";
+    let formData = req.body.formData;
+    formData = formData.split('&');
+
+    let cat = formData[0];
+    cat = cat.substr(cat.indexOf("=") + 1);
+    let name = formData[1];
+    name = name.substr(name.indexOf("=") + 1);
+    let title = formData[2];
+    title = title.substr(title.indexOf("=") + 1);
+    let prio = formData[3];
+    prio = prio.substr(prio.indexOf("=") + 1);
+
+    let dt = new Date();
+    let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let d = monthNames[dt.getMonth()] +' '+ dt.getDate() +','+ dt.getFullYear() +' '+ dt.getHours() +':'+ dt.getMinutes();
+
+    let dataTickets = [];
+    mongo.connect(mongourl, (err, db) => {
+        assert.equal(null, err);
+        let cursorTickets = db.collection('tickets').find({});
+        cursorTickets.forEach((dbitem, index, arr) => {
+            assert.equal(null, index);
+            dataTickets.push(dbitem);
+        }, () => {
+            let currNo = dataTickets[dataTickets.length - 1].number;
+
+            db.collection('tickets').insertOne({
+                number: currNo + 1,
+                date: d,
+                category: decodeURIComponent(cat),
+                name: decodeURIComponent(name),
+                title: decodeURIComponent(title),
+                priority: prio
+            });
+
+            db.close();
+            res.send('Success');
+        });
+    });
 });
 
 module.exports = router;
